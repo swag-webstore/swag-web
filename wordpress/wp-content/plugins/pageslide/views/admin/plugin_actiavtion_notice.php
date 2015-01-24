ad8bdf7cb7c99cb154be0e7df49836bd7e0d4405
motani <tanimannnn@gmail.com>
2015-01-24 07:10:50 +0000

    delete!

--- wordpress/wp-content/plugins/pageslide/views/admin/plugin_actiavtion_notice.php
+++
@@ -1,8 +0,0 @@
-<div class="updated">
-	<p>
-		<strong>
-			<?php _e( 'Enable and configure the page slide from the', 'wpps' ) ?>
-			<a href="<?php echo $settings_link ?>"><?php _e( 'settings', 'wpmp' ) ?></a>
-		</strong>
-	</p>
-</div>
